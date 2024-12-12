087c1674eb2c8cc48791daa036463254b7117a26
H9nt <63129066+h9nt@users.noreply.github.com>
2024-12-12 12:41:16 +0100

    Update sslpinning.java

--- utils/sslpinning.java
+++ utils/sslpinning.java
@@ -1,5 +1,8 @@
-OkHttpClient client = new OkHttpClient.Builder()
-    .certificatePinner(new CertificatePinner.Builder()
-        .add("yourdomain.com", "sha256/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=")
-        .build())
-    .build();
+public static OkHttpClient getPinnedHttpClient() {
+    return new OkHttpClient.Builder()
+        .certificatePinner(new CertificatePinner.Builder()
+            .add("yourdomain.com", "sha256/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=")
+            .build()
+        )
+        .build();
+}
