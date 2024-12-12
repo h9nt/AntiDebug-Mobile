92d690b91be48ca48533cf3329bba91f91ad9434
H9nt <63129066+h9nt@users.noreply.github.com>
2024-12-12 12:31:10 +0100

    Create isSignatureValid.java

---
+++ utils/isSignatureValid.java
@@ -0,0 +1,19 @@
+private boolean isSignatureValid(Context context) {
+    try {
+        PackageInfo packageInfo = context.getPackageManager().getPackageInfo(
+            context.getPackageName(),
+            PackageManager.GET_SIGNING_CERTIFICATES
+        );
+
+        String expectedSignature = "YOUR_EXPECTED_SIGNATURE_HASH"; // SHA-256 hash of your APK's certificate
+        String currentSignature = Base64.encodeToString(
+            packageInfo.signingInfo.getApkContentsSigners()[0].toByteArray(),
+            Base64.DEFAULT
+        );
+
+        return expectedSignature.equals(currentSignature);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return false;
+}
