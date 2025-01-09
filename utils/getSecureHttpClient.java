758e507477656d255a32f41de0588e649d0dcc33
H9nt <63129066+h9nt@users.noreply.github.com>
2025-01-09 18:52:09 +0100

    Update getSecureHttpClient.java

--- utils/getSecureHttpClient.java
+++ utils/getSecureHttpClient.java
@@ -2,7 +2,6 @@ public static OkHttpClient getSecureHttpClient() {
     return new OkHttpClient.Builder()
         .addInterceptor(chain -> {
             HttpUrl url = chain.request().url();
-            // Block requests to localhost
             if ("127.0.0.1".equals(url.host()) || "localhost".equals(url.host())) {
                 throw new IOException("Blocked localhost traffic");
             }
