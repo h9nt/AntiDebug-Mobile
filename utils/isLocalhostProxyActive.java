60147664f76b7fadbe24b32ef373b0a95cfc8241
H9nt <63129066+h9nt@users.noreply.github.com>
2025-01-09 18:54:08 +0100

    Update isLocalhostProxyActive.java

--- utils/isLocalhostProxyActive.java
+++ utils/isLocalhostProxyActive.java
@@ -1,10 +1,10 @@
 private boolean isLocalhostProxyActive() {
-    int[] commonPorts = {8080, 8888, 9090, 8000}; // Common proxy ports
+    int[] commonPorts = {8080, 8888, 9090, 8000};
     for (int port : commonPorts) {
         try {
             Socket socket = new Socket("127.0.0.1", port);
             socket.close();
-            return true; // Traffic is redirected to localhost
+            return true;
         } catch (IOException ignored) {
         }
     }
