1f232a9365ca8f30640f4179914820f2d6960cfe
H9nt <63129066+h9nt@users.noreply.github.com>
2025-01-09 18:53:18 +0100

    Update isFridaServerRunning.java

--- utils/isFridaServerRunning.java
+++ utils/isFridaServerRunning.java
@@ -2,7 +2,7 @@ private boolean isFridaServerRunning() {
     try {
         Socket socket = new Socket("127.0.0.1", 27042);
         socket.close();
-        return true; // Frida server detected
+        return true;
     } catch (IOException e) {
         return false;
     }
