2afb7927be9e2d8fc09dd93c11e40562dc6b10b1
H9nt <63129066+h9nt@users.noreply.github.com>
2025-01-09 18:53:38 +0100

    Update isHttpToolkitRunning.java

--- utils/isHttpToolkitRunning.java
+++ utils/isHttpToolkitRunning.java
@@ -1,6 +1,6 @@
 private boolean isHttpToolkitRunning() {
     try {
-        Socket socket = new Socket("127.0.0.1", 8000); // HTTP Toolkit uses port 8000
+        Socket socket = new Socket("127.0.0.1", 8000);
         socket.close();
         return true;
     } catch (IOException ignored) {
